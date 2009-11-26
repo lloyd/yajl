fa941bf0cab08f8b952206259969b165b2a8f9ff
Lloyd Trevor Hilaiel <lloyd@hilaiel.com>
2009-11-25 22:19:57 -0700

    a cast to keep gcc happy

--- src/yajl_encode.c
+++ src/yajl_encode.c
@@ -48,11 +48,11 @@ void
 yajl_string_encode(yajl_buf buf, const unsigned char * str,
                    unsigned int len)
 {
-    yajl_string_encode2(&yajl_buf_append, buf, str, len);
+    yajl_string_encode2((yajl_print_t) &yajl_buf_append, buf, str, len);
 }
 
 void
-yajl_string_encode2(const yajl_print* print,
+yajl_string_encode2(const yajl_print_t print,
                     void * ctx,
                     const unsigned char * str,
                     unsigned int len)
