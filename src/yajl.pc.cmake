3b00ede24613936e78754e5b3aabb0eec3bdc955
Eric Monti <monti@squareup.com>
2012-01-17 10:56:24 -0800

    includedir should be -I{prefix}/include also

--- src/yajl.pc.cmake
+++ src/yajl.pc.cmake
@@ -1,9 +1,9 @@
 prefix=${CMAKE_INSTALL_PREFIX}
 libdir=${dollar}{prefix}/lib${LIB_SUFFIX}
-includedir=${dollar}{prefix}/include/yajl
+includedir=${dollar}{prefix}/include
 
 Name: Yet Another JSON Library
 Description: A Portable JSON parsing and serialization library in ANSI C
 Version: ${YAJL_MAJOR}.${YAJL_MINOR}.${YAJL_MICRO}
-Cflags: -I${dollar}{includedir}
+Cflags: -I${dollar}{includedir} -I${dollar}{includedir}/yajl
 Libs: -L${dollar}{libdir} -lyajl
