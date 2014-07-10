681eeb01ac1cfd56b3a21a409cd64781d26798f0
Pietro Cerutti <gahr@gahr.ch>
2014-07-10 20:33:02 +0200

    The 'yajl/' path component is used by header files. We do not need that in pkg-config's includedir.

--- src/yajl.pc.cmake
+++ src/yajl.pc.cmake
@@ -1,6 +1,6 @@
 prefix=${CMAKE_INSTALL_PREFIX}
 libdir=${dollar}{prefix}/lib${LIB_SUFFIX}
-includedir=${dollar}{prefix}/include/yajl
+includedir=${dollar}{prefix}/include
 
 Name: Yet Another JSON Library
 Description: A Portable JSON parsing and serialization library in ANSI C
